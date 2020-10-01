1a5fdff35991c6b2720b2dd06b2e3b1d992f287c
Joel Lee <joel@Joels-MacBook-Pro.local>
2020-10-01 21:00:51 +0800

    Add config and Pong

--- lib/server.ex
+++ lib/server.ex
@@ -4,6 +4,7 @@ defmodule Server do
   """
 
   use Application
+  require Logger
 
   def start(_type, _args) do
     Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
@@ -13,19 +14,36 @@ defmodule Server do
   Listen for incoming connections
   """
   def listen() do
-    IO.puts("Your code goes here!")
-
     # Uncomment this block to pass the first stage
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, _client} = :gen_tcp.accept(socket)
-    socket |> read_line |> :gen_tcp.send(socket)
+    Logger.info("Accepting connections on 6379")
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    Logger.info("test")
+    serve(client)
+    Logger.info("Accepting")
+    loop_acceptor(socket)
+  end
+
+  defp serve(client) do
+    client
+    |> read_line()
+    |> write_line(client)
+    serve(client)
   end
 
   defp read_line(socket) do
     {:ok, data} = :gen_tcp.recv(socket, 0)
-    case data do
-      "PING" -> "+PONG\r\n"
-       _ -> :error
+    data
+  end
+
+  defp write_line(line, socket) do
+    case line do
+      "Ping\n" -> :gen_tcp.send(socket, "+PONG\r\n")
+       _ -> :gen_tcp.send(socket, "PONG\r\n")
     end
   end
 end
