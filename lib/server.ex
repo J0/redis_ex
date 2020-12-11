32bbab072086e996078dce53f2059966815e8ef3
Joel Lee <joel@Joels-MacBook-Pro.local>
2020-12-11 21:50:01 +0800

    Fix issue with ets

--- lib/server.ex
+++ lib/server.ex
@@ -2,11 +2,11 @@ defmodule Server do
   @moduledoc """
   Your implementation of a Redis server
   """
-
   use Application
   require Logger
 
   def start(_type, _args) do
+    :ets.new(:kv, [:set, :public, :named_table])
     Supervisor.start_link(
       [{Task.Supervisor, name: Server.TaskSupervisor}, {Task, fn -> Server.listen() end}],
       strategy: :one_for_one
@@ -17,7 +17,6 @@ defmodule Server do
   Listen for incoming connections
   """
   def listen() do
-    # Uncomment this block to pass the first stage
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
     Logger.info("Accepting connections on 6379")
     loop_acceptor(socket)
@@ -43,14 +42,30 @@ defmodule Server do
     data
   end
 
+  defp echo(socket, command_arr) do
+    echo_statement = Enum.at(command_arr, -2)
+    IO.inspect(echo_statement)
+    :gen_tcp.send(socket, "+#{echo_statement}\r\n")
+  end
+
+  defp set(command_arr) do
+    :ets.insert(:kv, {"somestuff", "adgf"})
+  end
+
+  defp get(command_arr) do
+    :ets.lookup(:kv, "")
+  end
   defp write_line(line, socket) do
     command_arr = String.split(line, "\r\n")
-    echo_statement = Enum.at(command_arr, -2)
-    IO.inspect(line)
-    IO.inspect(echo_statement)
-    case line do
-      "*1\r\n$4\r\nping\r\n" -> :gen_tcp.send(socket, "+PONG\r\n")
-      _ -> :gen_tcp.send(socket, "+#{echo_statement}\r\n")
+    command = Enum.at(command_arr, 2) |>String.downcase
+    IO.inspect(command_arr)
+    IO.inspect(command)
+    case command do
+      "ping" -> :gen_tcp.send(socket, "+PONG\r\n")
+      "set"-> set(command_arr)
+      "get"-> get(command_arr)
+      "echo"-> echo(socket,command_arr)
+      _ -> :gen_tcp.send(socket, "Nope")
     end
   end
 end
